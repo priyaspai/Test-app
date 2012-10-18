b9dc40eae3745f2f140d8d08effcd917bc8d5ee6
Priya Pai <priya.spai@tcs.com>
2012-10-18 11:49:22 +0530

    Select and Save Links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,16 +2,13 @@ class LinksController < ApplicationController
 
 
   def create
-    @tweet_url = CGI::unescape(params[:tweet_url])
-    @tweet_id = params[:tweet_id]
-    @link = Link.new(:url => @tweet_url)
-    @link.user_id = current_user.id
-    if @link.save
-      respond_to do |format|
-        format.js
-      end
-    else
-      flash.now[:error] = @link.errors.full_messages
+    @tweet_urls = params[:tweet_url]
+    @tweet_urls.each do |tweet|
+      @tweet_url = CGI::unescape(tweet)
+      #@tweet_id = params[:tweet_id]
+      @link = Link.new(:url => @tweet_url)
+      @link.user_id = current_user.id
+      @link.save
     end
   end
 end
