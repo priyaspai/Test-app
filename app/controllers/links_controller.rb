81fae4c3155ca89954c985637b10139d484b9543
Priya Pai <priya.spai@tcs.com>
2012-10-19 08:57:28 +0530

    Links saving code updated

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,9 +5,7 @@ class LinksController < ApplicationController
     @tweet_urls = params[:tweet_url]
     @tweet_urls.each do |tweet|
       @tweet_url = CGI::unescape(tweet)
-      #@tweet_id = params[:tweet_id]
-      @link = Link.new(:url => @tweet_url)
-      @link.user_id = current_user.id
+      @link = current_user.links.build(:url => @tweet_url)
       @link.save
     end
   end
